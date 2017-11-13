8a49c417f639bf2b8ecfa09bee007fa3182fb304
ohenne <henneb@convection.biocmplx.nbi.dk>
2017-11-13 12:49:40 +0100

    added cdo gather

--- cape_2d_parallel.sh
+++ cape_2d_parallel.sh
@@ -5,21 +5,15 @@ set -ex
 #PFAD=`pwd`
 
 FSTEM=test1plus4K #plus4K_detail
+ystart=0
+yend=100
 mkdir -p $modelo/$FSTEM/level2
-dim=320
-#INPFAD=/mnt/lustre01/scratch/m/m214106/${FSTEM}/
-#OUTPFAD=/mnt/lustre01/scratch/m/m214106/${FSTEM}/paste_d8-11/
 
 
-#cd $INPFAD
-
-#for N in $(seq 0 ${dim}) ; do #$(seq 1 ${dim}); do
-for N in $(seq 0 150) ; do
-#for N in $(seq 4) ; do
+for N in $(seq $ystart $yend) ; do
   NSTRING=$(printf %01d $N)
   dimstr=$(printf %01d $dim)  #neccesary?
-#  python calculate_cape_rivers_corrected_171104.py ${NSTRING} ${FSTEM} ${dimstr} &
-  python calc_cape_jan_olga.py ${FSTEM} ${NSTRING} 1 &
+  python calc_cape.py ${FSTEM} ${NSTRING} 1 &
 #  read -p "Continue (y/n)?" choice
 #  if [[ "$choice" == y ]]; then
 #  echo "hm"
@@ -28,5 +22,6 @@ for N in $(seq 0 150) ; do
 #  fi
   #python calculate_cape_rivers_jan.py ${NSTRING}  &
 done 
-
+wait 
+cdo gather test1plus4K.cape.river_y_???.nc test1plus4K.cape.river_${ystart}_${yend}.nc 
 
